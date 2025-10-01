4ea5d41a13e2a9979d7f1f0844e7d8141499fe2d
pav89dev <pavanmdaryanani@gmail.com>
2025-10-01 07:43:40 +0700

    Delete check_runtime.js

--- check_runtime.js
+++
@@ -1,9 +0,0 @@
-// Check Node.js version
-const semver = require("semver");
-const required = ">=18.0.0";
-if (!semver.satisfies(process.version, required)) {
-  console.error(`❌ Node.js version ${process.version} does not satisfy ${required}`);
-  process.exit(1);
-} else {
-  console.log(`✅ Node.js version ${process.version} OK`);
-}
